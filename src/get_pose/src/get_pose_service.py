e8d7037370fa11c1ec31cbfe3aeea3c44980f57e
John Willes <johnwilles@gmail.com>
2018-07-02 20:32:07 -0400

    Add pose service logic

--- src/get_pose/src/get_pose_service.py
+++ src/get_pose/src/get_pose_service.py
@@ -1,12 +1,24 @@
 #! /usr/bin/env python
 
 import rospy
-from std_srvs.srv import Empty, EmptyReponse
+from std_srvs.srv import Empty, EmptyResponse
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose 
 
 
-def callback(request):
+
+def service_callback(request):
+    global robot_pose
+    print robot_pose
+    return EmptyResponse()
     
+    
+def sub_callback(msg):
+    global robot_pose
+    robot_pose = msg.pose.pose
     
 
 rospy.init_node('get_pose_service')
-get_pose_service = rospy.Service('/get_pose_service', Empty, callback)
+robot_pose = Pose()
+pose_client = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, sub_callback)
+get_pose_service = rospy.Service('/get_pose_service', Empty, service_callback)
+rospy.spin()
